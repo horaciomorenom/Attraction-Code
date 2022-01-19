2cfae2117ed12662d923c69eb1dad64c5260ea5a
Horacio Moreno <hmorenom@umich.edu>
2022-01-18 19:33:59 -0500

    Added some visual elements

--- orb.py
+++ orb.py
@@ -7,6 +7,28 @@ def normalize(v):
     return v/norm
 
 class orb:
+    """
+    A class used to represent an orb in the 2-D attraction experiment
+
+    ...
+
+    Attributes
+    ----------
+    position : np.ndarray
+        2-vector specifying the particle's position
+    name : str
+        the name of the animal
+    sound : str
+        the sound that the animal makes
+    num_legs : int
+        the number of legs the animal has (default 4)
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+    """
+
     def __init__(self, position):
         self.position = position
         self.velocity = np.array([0,0]) 
@@ -25,6 +47,10 @@ class orb:
     def update_position(self, timestep):
         self.position = self.position + self.velocity*timestep
         self.positions = np.append(self.positions, np.array([self.position]), axis=0)
+        self.x_positions = np.append(self.x_positions, self.position[0])
+        self.y_positions = np.append(self.y_positions, self.position[1])
+
     def calculate_distance_travelled(self):
         self.distance = np.sum(np.sqrt(np.sum(np.diff(self.positions, axis=0)**2, axis=1)))
+
 
